63f90272e47c6e6f4e7a1a77d226ed4259fcbac5
Varun Singh <varunsingh7004@gmail.com>
2025-08-26 12:54:49 +0530

    add more imp routes

--- apps/services/api-server/src/routes/auth.routes.ts
+++ apps/services/api-server/src/routes/auth.routes.ts
@@ -6,5 +6,12 @@ const router: Router = Router();
 router.route("/signup").post(signUp);
 router.route("/signin").post(signIn);
 router.route("/verify/:token").get(verifyUser);
+router.route("/me").get();
+router.route("/logout").post();
+
+router.route("/refresh-token").post();
+router.route("/forgot-password").post();
+router.route("/reset-password/:token").post();
+router.route("/change-password").post();
 
 export default router;
